016402fb90c3925442fdbdb55e446a0434ae2b1d
jameela alrashidi <22jameelaa22@gmail.com>
2022-08-10 16:08:10 +0300

    Php

---
+++ send.php
@@ -0,0 +1,32 @@
+<?php
+
+
+if (isset($_POST['number'])) {
+	include 'db_conn.php';
+
+	function validate($data){
+       $data = trim($data);
+	   $data = stripslashes($data);
+	   $data = htmlspecialchars($data);
+	   return $data;
+	}
+
+	$number = validate($_POST['number']);
+
+	if ( empty($number)) {
+		header("Location: index.html");
+	}else {
+
+		$sql = "INSERT INTO tests(number) VALUES('$number')";
+		$res = mysqli_query($conn, $sql);
+
+		if ($res) {
+			echo "Your message was sent successfully!";
+		}else {
+			echo "Your message could not be sent!";
+		}
+	}
+
+}else {
+	header("Location: index.html");
+}
